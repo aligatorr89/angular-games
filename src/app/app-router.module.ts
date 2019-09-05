413810958dcdfdd1594ba29d599d8739351c1e0e
Rok Spacapan <rok.spacapan@sledat.com>
2019-09-05 16:24:30 +0200

    angular-router added

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+
+export const routes: Route[] = [
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRouterModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { GamesModule } from './games/games.module';
@@ -10,7 +11,8 @@ import { GamesModule } from './games/games.module';
   ],
   imports: [
     BrowserModule,
-    GamesModule
+    AppRouterModule,
+    GamesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
