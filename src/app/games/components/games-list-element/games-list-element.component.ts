98e90ba17bc7d1deb26df4395eb5d0b331cd1e26
Rok Spacapan <rok.spacapan@sledat.com>
2019-09-06 04:24:29 +0200

    styling navbar

--- src/app/games/components/games-list-element/games-list-element.component.ts
+++ src/app/games/components/games-list-element/games-list-element.component.ts
@@ -4,7 +4,7 @@ import { Game } from '../../../shared/models/game';
 @Component({
   selector: 'app-games-list-element',
   templateUrl: './games-list-element.component.html',
-  styleUrls: ['./games-list-element.component.css']
+  styleUrls: ['./games-list-element.component.scss']
 })
 export class GamesListElementComponent implements OnInit {
 
