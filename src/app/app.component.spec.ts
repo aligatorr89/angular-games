df9decd1041fb2914224cdb64d84b50a13f0500a
Rok Spacapan <rok.spacapan@sledat.com>
2019-09-08 03:52:13 +0200

    jest migration ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }    from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppRouterModule, routes } from './app-router.module';
 import { AppComponent } from './app.component';
+import { GamesModule } from './games/games.module';
+import { HomePageComponent } from './home-page-components/home-page/home-page.component';
+import { HomePageHeaderComponent } from './home-page-components/home-page-header/home-page-header.component';
+import { HomePageContentComponent } from './home-page-components/home-page-content/home-page-content.component';
+import { HomePageFooterComponent } from './home-page-components/home-page-footer/home-page-footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomePageComponent,
+        HomePageHeaderComponent,
+        HomePageContentComponent,
+        HomePageFooterComponent
       ],
+      imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        RouterModule,
+        AppRouterModule,
+        RouterTestingModule,
+        GamesModule
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     }).compileComponents();
   }));
 
@@ -14,18 +44,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-games'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-games');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-games!');
   });
 });
