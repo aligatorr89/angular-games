47c48f9c91eb828fc74d690fef01d58f0c5737de
Rok Spacapan <rok.spacapan@sledat.com>
2019-09-09 08:07:16 +0200

    input-name and select-category unit tests

--- src/app/games/components/games-header/games-header.component.spec.ts
+++ src/app/games/components/games-header/games-header.component.spec.ts
@@ -2,10 +2,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { GamesHeaderComponent } from './games-header.component';
+import { ComponentTestInit } from '../../../shared/test/test-util';
 
 describe('GamesHeaderComponent', () => {
-  let component: GamesHeaderComponent;
-  let fixture: ComponentFixture<GamesHeaderComponent>;
+  let test: ComponentTestInit<GamesHeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,12 +16,32 @@ describe('GamesHeaderComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GamesHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    test = new ComponentTestInit(GamesHeaderComponent);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(test.component).toBeTruthy();
+  });
+
+  it('should call searchByName method when type into Search game input', () => {
+    const spy = jest.spyOn(test.component, 'searchByName');
+
+    const input: HTMLInputElement = test.html.querySelector('input[name="search-game"]');
+    input.value = 'w';
+    const event = new KeyboardEvent("keyup", {key : "w"});
+    input.dispatchEvent(event);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call changeCategory() method when select from drowdown', () => {
+    const spy = jest.spyOn(test.component, 'changeCategory');
+
+    const select: HTMLSelectElement = test.html.querySelector('select[name="select-category"]');
+    const event = new Event('change');
+    select.selectedIndex = 3;
+    select.dispatchEvent(event);
+    
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
